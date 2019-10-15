5bc2781bf03fb9651be90f7019c237c740a8dfcc
Kyle Spearrin <kyle.spearrin@gmail.com>
2019-10-14 21:55:35 -0400

    update build

--- SharedAssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTitle("BerTlv.NET")]
-[assembly: AssemblyDescription("A library for parsing BER TLV data.")]
-[assembly: AssemblyCompany("Kyle Spearrin")]
-[assembly: AssemblyProduct("BerTlv.NET")]
-[assembly: AssemblyCopyright("Copyright (C) Kyle Spearrin 2016")]
-[assembly: AssemblyVersion("2.0.0")]
