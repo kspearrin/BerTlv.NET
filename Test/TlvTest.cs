e7dfe0493ce9b963590682b5e67b213183a838e5
bepursuant <kklika@bepursuant.com>
2019-11-05 18:59:41 -0600

    Modified test to add inline and trailing padding (0x00 and 0xFFFF)

--- Test/TlvTest.cs
+++ Test/TlvTest.cs
@@ -9,28 +9,56 @@ namespace Test
     public class TlvTest
     {
         // Verify: http://www.emvlab.org/tlvutils/?data=6F1A840E315041592E5359532E4444463031A5088801025F2D02656E
-        private string _emvHexTlv = "6F1A840E315041592E5359532E4444463031A5088801025F2D02656E";
+        private string _validAsciiHexString = "6F1A840E315041592E5359532E4444463031A5088801025F2D02656E";
+
+        // Note: emvlab does not correctly parse inline and trailing padding (0x00, 0xFF), but the specification 
+        // allows for it. The TLV string below should be parsed in the same way as the one above
+        private string _validAsciiHexStringWithPadding = "006F1B840E315041592E5359532E444446303100A5088801025F2D02656EFFFF";
 
         [Fact]
         [Trait("Build", "Run")]
-        public void ParseEmvHexTest()
+        public void ShouldParseValidAsciiHexString()
         {
-            var tlvs = Tlv.ParseTlv(_emvHexTlv);
+            var tlvs = Tlv.ParseTlv(_validAsciiHexString);
 
             AssertTlv(tlvs);
         }
 
         [Fact]
         [Trait("Build", "Run")]
-        public void ParseEmvByteTest()
+        public void ShouldParseValidHexArray()
         {
-            var emvBytes = Enumerable
-                .Range(0, _emvHexTlv.Length)
+            var validHexArray = Enumerable
+                .Range(0, _validAsciiHexString.Length)
                 .Where(x => x % 2 == 0)
-                .Select(x => Convert.ToByte(_emvHexTlv.Substring(x, 2), 16))
+                .Select(x => Convert.ToByte(_validAsciiHexString.Substring(x, 2), 16))
                 .ToArray();
 
-            var tlvs = Tlv.ParseTlv(emvBytes);
+            var tlvs = Tlv.ParseTlv(validHexArray);
+
+            AssertTlv(tlvs);
+        }
+
+        [Fact]
+        [Trait("Build", "Run")]
+        public void ShouldParseValidAsciiHexStringWithPadding()
+        {
+            var tlvs = Tlv.ParseTlv(_validAsciiHexStringWithPadding);
+
+            AssertTlv(tlvs);
+        }
+
+        [Fact]
+        [Trait("Build", "Run")]
+        public void ShouldParseValidHexArrayStringWithPadding()
+        {
+            var validHexArrayWithPadding = Enumerable
+                .Range(0, _validAsciiHexStringWithPadding.Length)
+                .Where(x => x % 2 == 0)
+                .Select(x => Convert.ToByte(_validAsciiHexStringWithPadding.Substring(x, 2), 16))
+                .ToArray();
+
+            var tlvs = Tlv.ParseTlv(validHexArrayWithPadding);
 
             AssertTlv(tlvs);
         }
